84ef813ebbe08e30be7064484570c120bddfede0
Mario Henderson <33106146+MarioDragon@users.noreply.github.com>
2017-10-29 02:36:57 -0600

    Update Judging Moose.cpp

--- Judging Moose.cpp
+++ Judging Moose.cpp
@@ -11,19 +11,15 @@ int main()
 	if (l == 0 && r == 0)
 		cout << "Not a moose";
 	else if (l == r)
-	{
 		cout << "Even " << r * 2;
-
-	}
 	else if (l != r)
-		{
-			cout << "Odd ";
+	{
+		cout << "Odd ";
 			if (l < r)
 				cout << r * 2;
 			else
 				cout << l * 2;
-		}
+	}
 
 	return 0;
-
 }
