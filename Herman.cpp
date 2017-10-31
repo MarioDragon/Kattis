afb23d64d7a58d3c8035d9d50d4b67993e92df19
Mario Henderson <33106146+MarioDragon@users.noreply.github.com>
2017-10-31 01:27:39 -0600

    Add files via upload

---
+++ Herman.cpp
@@ -0,0 +1,27 @@
+#define _USE_MATH_DEFINES
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+//no clue why this works but it does
+//compares normal geometry to taxicab geometry
+using namespace std;
+
+int main()
+{
+	double pi = M_PI; //you can set pi to whatever you want realisitcally
+	double radius;
+	double normal, taxicab;
+
+	cin >> radius;
+
+	normal = pi * pow(radius, 2);
+
+	taxicab = 4 * pow(radius, 2);
+	taxicab = taxicab / 2;
+
+	cout << fixed << setprecision(6);
+	cout << normal << endl;
+	cout << taxicab << endl;
+
+	return 0;
+}
